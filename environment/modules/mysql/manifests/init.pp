59d706b99f76b27458f9b2a0ae1a08d86af5cec3
Martin Westergaard Lassen <mwl@trifork.com>
2012-07-05 13:14:26 +0200

    Bootstrap sample database

--- environment/modules/mysql/manifests/init.pp
+++ environment/modules/mysql/manifests/init.pp
@@ -9,4 +9,15 @@ class mysql() {
         hasrestart => true,
         hasstatus => true
     }
+
+    file {"/tmp/bootstrap.sql":
+        ensure => present,
+        content => template('mysql/bootstrap.sql')
+    }
+
+    exec { "bootstrap-db":
+        command => "mysql -u root < /tmp/bootstrap.sql",
+        require => [File["/tmp/bootstrap.sql"], Service["mysql"]],
+        unless => 'test `mysql -u root <<< "SHOW DATABASES;" | grep sdm-sample | wc -l` -eq 0'
+    }
 }
---
+++ environment/modules/mysql/templates/bootstrap.sql
@@ -0,0 +1,3 @@
+CREATE USER 'sdmsample'@'localhost' IDENTIFIED BY '';
+CREATE DATABASE sdmsample;
+GRANT ALL PRIVILEGES ON sdmsample.* TO 'sdmsample'@'localhost';
