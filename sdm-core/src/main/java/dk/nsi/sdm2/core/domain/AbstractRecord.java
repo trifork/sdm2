b6ac34e46defc696c1c61e0d182cf7558c6d43e1
Martin Westergaard Lassen <mwl@trifork.com>
2012-07-23 13:44:46 +0200

    More necessary fields on domain object AbstractRecord

--- sdm-core/src/main/java/dk/nsi/sdm2/core/domain/AbstractRecord.java
+++ sdm-core/src/main/java/dk/nsi/sdm2/core/domain/AbstractRecord.java
@@ -1,9 +1,41 @@
 package dk.nsi.sdm2.core.domain;
 
+import org.joda.time.DateTime;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 @Entity
+@MappedSuperclass
 public abstract class AbstractRecord {
+    @Id
+    private Long id;
 
+    protected DateTime validFrom;
+    protected DateTime modifiedDate;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DateTime getValidFrom() {
+        return validFrom;
+    }
+
+    public void setValidFrom(DateTime validFrom) {
+        this.validFrom = validFrom;
+    }
+
+    public DateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(DateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
 }
