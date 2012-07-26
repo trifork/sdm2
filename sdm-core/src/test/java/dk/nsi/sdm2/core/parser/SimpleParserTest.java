2fa6976cbbad5e92af8785f02dafdceca64e05e6
mwl <mwl@trifork.com>
2012-07-26 08:48:51 +0200

    fixed import

--- sdm-core/src/test/java/dk/nsi/sdm2/core/parser/SimpleParserTest.java
+++ sdm-core/src/test/java/dk/nsi/sdm2/core/parser/SimpleParserTest.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
