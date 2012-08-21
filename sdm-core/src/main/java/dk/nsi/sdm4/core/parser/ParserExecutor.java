1969cef051b86cb570e78cda31619a03b06f5a95
Jakob Færch <jrf@trifork.com>
2012-08-21 13:37:33 +0200

    Vi ønsker ikke slalogning af operationen "jeg kiggede i datadir, og der var ingenting", så vi logger kun hvis der er udført en reel parsning af data eller der skete en fejl da vi prøvede at tjekke datadir

--- sdm-core/src/main/java/dk/nsi/sdm4/core/parser/ParserExecutor.java
+++ sdm-core/src/main/java/dk/nsi/sdm4/core/parser/ParserExecutor.java
@@ -2,13 +2,11 @@ package dk.nsi.sdm4.core.parser;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import dk.nsi.sdm4.core.config.StamdataConfiguration;
 import dk.sdsd.nsp.slalog.api.SLALogItem;
 import dk.sdsd.nsp.slalog.api.SLALogger;
 
@@ -26,23 +24,28 @@ public class ParserExecutor {
     
     @Scheduled(fixedDelay = 1000)
     public void run() {
-        // TODO logging
         String parserIdentifier = parser.getHome();
         SLALogItem slaLogItem = slaLogger.createLogItem("ParserExecutor", "Executing parser " + parserIdentifier);
-        
-//        URL resource = this.getClass().getResource("/log4j.xml");
-//        if(resource != null) {
-//            System.out.println(resource.toExternalForm());
-//            logger.fatal(resource.toExternalForm());
-//        } else {
-//            logger.fatal("log4j.properties not found");
-//        }
-        
+
         try {
-            runParserOnInbox();
-            
-            slaLogItem.setCallResultOk();
-            slaLogItem.store();
+	        if(logger.isDebugEnabled()) {
+	            logger.debug("Running parser " + parser.getHome());
+	        }
+
+	        inbox.update();
+	        File dataSet = inbox.top();
+
+	        if (dataSet != null && !inbox.isLocked()) {
+	            parser.process(dataSet);
+
+	            // Once the import is complete
+	            // we can remove of the data set
+	            // from the inbox.
+	            inbox.advance();
+
+		        slaLogItem.setCallResultOk();
+		        slaLogItem.store();
+	        } // if there is no data and no error, we never call store on the log item, which is okay
         } catch (Exception e) {
             slaLogItem.setCallResultError("Parser " + parserIdentifier + " failed - Cause: " + e.getMessage());
             slaLogItem.store();
