4ab75a966b312a8404e83e332b377b211ba9f78e
Jakob Færch <jrf@trifork.com>
2012-08-23 12:38:09 +0200

    Flyttet sdm-core til https://github.com/trifork/sdm4-core

--- sdm-core/src/main/java/dk/nsi/sdm4/core/config/StamdataConfigurationSupport.java
+++
@@ -1,9 +0,0 @@
-package dk.nsi.sdm4.core.config;
-
-import dk.nsi.sdm4.core.parser.Parser;
-import org.springframework.context.annotation.Bean;
-
-public interface StamdataConfigurationSupport {
-    @Bean
-    public Parser parser();
-}
--- sdm-core/src/main/java/dk/nsi/sdm4/core/status/TimeSource.java
+++
@@ -1,9 +0,0 @@
-package dk.nsi.sdm4.core.status;
-
-import org.joda.time.DateTime;
-
-import javax.xml.crypto.Data;
-
-public interface TimeSource {
-	DateTime now();
-}
