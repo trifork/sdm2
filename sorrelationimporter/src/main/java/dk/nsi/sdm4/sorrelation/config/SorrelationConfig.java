b0cb4563f8618b0b53d2cc8b1f0c1e760cf7ac4c
Jakob Færch <jrf@trifork.com>
2012-08-23 14:08:26 +0200

    SORRelation-importer jboss deployment + statusside virker

--- sorrelationimporter/src/main/java/dk/nsi/sdm4/sorrelation/config/SorrelationConfig.java
+++ sorrelationimporter/src/main/java/dk/nsi/sdm4/sorrelation/config/SorrelationConfig.java
@@ -2,6 +2,7 @@ package dk.nsi.sdm4.sorrelation.config;
 
 import dk.nsi.sdm4.core.config.StamdataConfiguration;
 import dk.nsi.sdm4.core.parser.Parser;
+import dk.nsi.sdm4.core.parser.ParserException;
 import dk.nsi.sdm4.core.persistence.AuditingPersister;
 import dk.nsi.sdm4.core.persistence.Persister;
 import dk.sdsd.nsp.slalog.api.SLALogConfig;
@@ -9,14 +10,29 @@ import dk.sdsd.nsp.slalog.api.SLALogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.io.File;
 import java.sql.SQLException;
 
 @Configuration
+@EnableScheduling
+@EnableTransactionManagement
 public class SorrelationConfig extends StamdataConfiguration {
 	@Bean
 	public Parser parser() {
-		return null;
+		return new Parser() {
+			@Override
+			public void process(File dataSet) throws ParserException {
+				throw new UnsupportedOperationException("process");
+			}
+
+			@Override
+			public String getHome() {
+				return "sorrelationimporter";
+			}
+		};
 	}
 
 	// this is not automatically registered, see https://jira.springsource.org/browse/SPR-8539
