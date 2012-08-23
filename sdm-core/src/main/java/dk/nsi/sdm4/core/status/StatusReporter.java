c4043b9cb280410235325e0f5573586247595c99
Kjeld Froberg <kfr@trifork.com>
2012-08-23 08:26:12 +0200

    Statusreporter checker for sidste udfald af importeren

--- sdm-core/src/main/java/dk/nsi/sdm4/core/status/StatusReporter.java
+++ sdm-core/src/main/java/dk/nsi/sdm4/core/status/StatusReporter.java
@@ -9,12 +9,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import dk.nsi.sdm4.core.parser.Inbox;
+import dk.nsi.sdm4.core.status.ImportStatus.Outcome;
 
 @Controller
 public class StatusReporter {
     
     @Autowired
     Inbox inbox;
+
+    @Autowired
+    ImportStatusRepository statusRepo;
 
     @RequestMapping(value = "/status")
     public ResponseEntity<String> reportStatus() {
@@ -25,6 +29,8 @@ public class StatusReporter {
         try {
             if(inbox.isLocked()) {
                 body = "Inbox is locked: " + inbox;
+            } else if(lastStatusIsFailure()) {
+                // status applied later
             } else {
                 status = HttpStatus.OK;
             }
@@ -32,7 +38,33 @@ public class StatusReporter {
             body = e.getMessage();
         }
 
+        body = addLastRunInformation(body);
+        
         headers.setContentType(MediaType.TEXT_PLAIN);
         return new ResponseEntity<String>(body, headers, status);
+    }
+
+    private boolean lastStatusIsFailure() {
+        if (statusRepo != null && 
+            statusRepo.getLatestStatus() != null && 
+            statusRepo.getLatestStatus().getOutcome() != null && 
+            statusRepo.getLatestStatus().getOutcome().equals(Outcome.FAILURE)) {
+            return true;
+        }
+        return false;
+    }
+
+    private String addLastRunInformation(String body) {
+        ImportStatus latestStatus = statusRepo.getLatestStatus();
+        if (latestStatus == null) {
+            return body += "\nLast import: Never run";
+        } else {
+            body += "\nLast import ended at: " + latestStatus.getEndTime();
+            Outcome outcome = latestStatus.getOutcome();
+            if(outcome != null && outcome.equals(Outcome.FAILURE)) {
+                body += "with outcome" + Outcome.FAILURE;
+            }
+            return body;
+        }
     }
 }
--- sdm-core/src/test/java/dk/nsi/sdm4/core/status/StatusReporterTest.java
+++ sdm-core/src/test/java/dk/nsi/sdm4/core/status/StatusReporterTest.java
@@ -1,10 +1,12 @@
 package dk.nsi.sdm4.core.status;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -12,6 +14,7 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import dk.nsi.sdm4.core.parser.Inbox;
 import dk.nsi.sdm4.core.status.StatusReporter;
+import dk.nsi.sdm4.core.status.ImportStatus.Outcome;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -25,11 +28,17 @@ public class StatusReporterTest {
     static class TestConfiguration {
         @Bean
         public Inbox inbox() {
-            return mock(Inbox.class);
+            Inbox inbox = mock(Inbox.class);
+            return inbox;
+
         }
         @Bean
         public StatusReporter statusReporter() {
             return new StatusReporter();
+        }
+        @Bean ImportStatusRepository importStatusRepository() {
+            ImportStatusRepository repo = mock(ImportStatusRepository.class);
+            return repo;
         }
     }
 
@@ -37,15 +46,17 @@ public class StatusReporterTest {
     Inbox inbox;
     @Autowired
     StatusReporter reporter;
+    @Autowired
+    ImportStatusRepository statusRepo;
 
+    
     @Test
-    public void willReturk200OK() throws Exception {
+    public void willReturn200underNormalCircumstances() throws Exception {
         when(inbox.isLocked()).thenReturn(false);
-
         final ResponseEntity<String> response = reporter.reportStatus();
 
         assertEquals(200, response.getStatusCode().value());
-        assertEquals("OK", response.getBody());
+        assertTrue(response.getBody().startsWith("OK"));
     }
 
     @Test
@@ -56,5 +67,46 @@ public class StatusReporterTest {
 
         assertEquals(500, response.getStatusCode().value());
         assertTrue(response.getBody().startsWith("Inbox is locked"));
+    }    
+    
+    @Test
+    public void reportsNeverRunWhenImportStatusDoesNotExist() {
+        when(statusRepo.getLatestStatus()).thenReturn(null);
+        when(inbox.isLocked()).thenReturn(false);
+        
+        final ResponseEntity<String> response = reporter.reportStatus();
+        assertEquals(200, response.getStatusCode().value());
+        assertTrue(response.getBody().toLowerCase().contains("never run"));
+    }
+    
+    @Test
+    public void reportsEndTimeOfLastSuccess() {
+        ImportStatus status = new ImportStatus();
+        DateTime endTime = new DateTime();
+        status.setEndTime(endTime);
+        status.setOutcome(Outcome.SUCCESS);
+        
+        when(statusRepo.getLatestStatus()).thenReturn(status);
+        when(inbox.isLocked()).thenReturn(false);
+        
+        final ResponseEntity<String> response = reporter.reportStatus();
+        assertEquals(200, response.getStatusCode().value());
+        
+        assertTrue(response.getBody().contains(endTime.toString()));
+    }
+
+    @Test
+    public void reportsLastFailure() {
+        ImportStatus status = new ImportStatus();
+        DateTime endTime = new DateTime();
+        status.setEndTime(endTime);
+        status.setOutcome(Outcome.FAILURE);
+        
+        when(statusRepo.getLatestStatus()).thenReturn(status);
+        when(inbox.isLocked()).thenReturn(false);
+        
+        final ResponseEntity<String> response = reporter.reportStatus();
+        assertEquals(500, response.getStatusCode().value());
+        assertTrue(response.getBody().contains(Outcome.FAILURE.toString()));
     }
 }
