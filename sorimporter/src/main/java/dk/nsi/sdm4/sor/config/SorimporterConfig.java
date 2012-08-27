e0084785b36c6192075df7ab6ac9da029cacc42e
Jakob Færch <jrf@trifork.com>
2012-08-27 11:16:49 +0200

    Initielt skelet til sorimporter

---
+++ sorimporter/src/main/java/dk/nsi/sdm4/sor/config/SorimporterConfig.java
@@ -0,0 +1,53 @@
+package dk.nsi.sdm4.sor.config;
+
+import dk.nsi.sdm4.core.config.StamdataConfiguration;
+import dk.nsi.sdm4.core.parser.Parser;
+import dk.nsi.sdm4.core.parser.ParserException;
+import dk.nsi.sdm4.core.persistence.AuditingPersister;
+import dk.nsi.sdm4.core.persistence.Persister;
+import dk.sdsd.nsp.slalog.api.SLALogConfig;
+import dk.sdsd.nsp.slalog.api.SLALogger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.io.File;
+import java.sql.SQLException;
+
+@Configuration
+@EnableScheduling
+@EnableTransactionManagement
+public class SorimporterConfig extends StamdataConfiguration {
+	@Bean
+	public Parser parser() {
+		return new Parser() {
+			@Override
+			public void process(File dataSet) throws ParserException {
+				throw new UnsupportedOperationException("process");
+			}
+
+			@Override
+			public String getHome() {
+				return "sorimporter";
+			}
+		};
+	}
+
+	@Bean
+	public Persister persister() throws SQLException {
+		return new AuditingPersister();
+	}
+
+	// this is not automatically registered, see https://jira.springsource.org/browse/SPR-8539
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+	    return StamdataConfiguration.propertySourcesPlaceholderConfigurer();
+    }
+
+	@Bean
+	public SLALogger slaLogger() {
+		return new SLALogConfig("Stamdata SOR-importer", "sorimporter").getSLALogger();
+	}
+}
---
+++ sorimporter/src/main/resources/db/migration/V20120827_1035__ImporterStatus.sql
@@ -0,0 +1,10 @@
+
+
+CREATE TABLE sorimporterImportStatus (
+    Id BIGINT(15) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    StartTime DATETIME NOT NULL,
+    EndTime DATETIME,
+    Outcome VARCHAR(20),
+
+    INDEX (StartTime)
+);
