60e3420f09cc5de11c99149fc6a791f50fb6fe30
Jakob Færch <jrf@trifork.com>
2012-08-31 09:50:35 +0200

    ryddet op i imports

--- autorisationimporter/src/main/java/dk/nsi/sdm4/autorisation/config/AutorisationInfrastructureConfig.java
+++ autorisationimporter/src/main/java/dk/nsi/sdm4/autorisation/config/AutorisationInfrastructureConfig.java
@@ -1,15 +1,10 @@
 package dk.nsi.sdm4.autorisation.config;
 
-import dk.nsi.sdm4.autorisation.parser.AutorisationParser;
 import dk.nsi.sdm4.core.config.StamdataConfiguration;
-import dk.nsi.sdm4.core.parser.Parser;
-import dk.nsi.sdm4.core.persistence.AuditingPersister;
-import dk.nsi.sdm4.core.persistence.Persister;
 import dk.sdsd.nsp.slalog.api.SLALogConfig;
 import dk.sdsd.nsp.slalog.api.SLALogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
