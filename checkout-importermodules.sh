958288e637674f34c66f2040fb0c7d0e091d7f38
Jakob Færch <jrf@trifork.com>
2012-09-04 13:32:24 +0200

    Ved at flytte doseringimporter til https://github.com/trifork/sdm4-doseringimporter/

--- checkout-importermodules.sh
+++ checkout-importermodules.sh
@@ -8,5 +8,6 @@ git clone git@github.com:trifork/sdm4-sorimporter.git
 git clone git@github.com:trifork/sdm4-sorrelationimporter.git
 git clone git@github.com:trifork/sdm4-takstimporter.git
 git clone git@github.com:trifork/sdm4-yderimporter.git
+git clone git@github.com:trifork/sdm4-doseringimporter.git
 git clone git@github.com:trifork/sdm4-testutils.git
 
