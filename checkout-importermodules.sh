d29e2e3452b5afb7a692a2cfd4df623e690f4787
Jakob Færch <jrf@trifork.com>
2012-09-04 12:26:34 +0200

    Flyttet cprimporter til https://github.com/trifork/sdm4-cprimporter/

--- checkout-importermodules.sh
+++ checkout-importermodules.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 git clone git@github.com:trifork/sdm4-autorisationimporter.git
 git clone git@github.com:trifork/sdm4-bemyndigelseimporter.git
+git clone git@github.com:trifork/sdm4-cprimporter.git
