20f6e957d4b4447c0c5550c196bc901918fbb78a
Jakob Færch <jrf@trifork.com>
2012-09-04 12:40:51 +0200

    Flyttet sksimporter til https://github.com/trifork/sdm4-sksimporter/

--- checkout-importermodules.sh
+++ checkout-importermodules.sh
@@ -3,3 +3,4 @@ git clone git@github.com:trifork/sdm4-autorisationimporter.git
 git clone git@github.com:trifork/sdm4-bemyndigelseimporter.git
 git clone git@github.com:trifork/sdm4-cprimporter.git
 git clone git@github.com:trifork/sdm4-sikredeimporter.git
+git clone git@github.com:trifork/sdm4-sksimporter.git
