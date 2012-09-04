496473af6a7545a3c75c4930119c4222f52f1e23
Jakob Færch <jrf@trifork.com>
2012-09-04 12:58:31 +0200

    Flyttet takstimporter til https://github.com/trifork/sdm4-takstimporter/

--- checkout-importermodules.sh
+++ checkout-importermodules.sh
@@ -6,4 +6,4 @@ git clone git@github.com:trifork/sdm4-sikredeimporter.git
 git clone git@github.com:trifork/sdm4-sksimporter.git
 git clone git@github.com:trifork/sdm4-sorimporter.git
 git clone git@github.com:trifork/sdm4-sorrelationimporter.git
-
+git clone git@github.com:trifork/sdm4-takstimporter.git
--- takstimporter/src/main/resources/db/migration/V20120829_1228__Takstimporter_ImporterStatus.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE takstimporterImportStatus (
-    Id BIGINT(15) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    StartTime DATETIME NOT NULL,
-    EndTime DATETIME,
-    Outcome VARCHAR(20),
-
-    INDEX (StartTime)
-);
