acfcaba4f35510b9eb12ab6cbc40f3cb70490dd2
Jakob Færch <jrf@trifork.com>
2012-09-04 13:11:37 +0200

    Flyttet takstimporter til https://github.com/trifork/sdm4-yderimporter/

--- checkout-importermodules.sh
+++ checkout-importermodules.sh
@@ -7,3 +7,5 @@ git clone git@github.com:trifork/sdm4-sksimporter.git
 git clone git@github.com:trifork/sdm4-sorimporter.git
 git clone git@github.com:trifork/sdm4-sorrelationimporter.git
 git clone git@github.com:trifork/sdm4-takstimporter.git
+git clone git@github.com:trifork/sdm4-yderimporter.git
+
